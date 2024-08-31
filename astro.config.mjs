6b8cefea18375a9651807e3433498ad63da1319b
Anish <xevenbiswas@gmail.com>
2024-09-01 02:36:18 +0530

    working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, sharpImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  image: sharpImageService(),
   site: "https://astrojs.org",
   integrations: [tailwind({ applyBaseStyles: false, }), mdx(), sitemap(), icon(), react()],
 });
