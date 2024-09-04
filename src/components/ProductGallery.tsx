f7b7dfc71153c8601a6549e33671733f415bbfab
Anish <xevenbiswas@gmail.com>
2024-09-04 23:00:15 +0530

    fix product

--- src/components/ProductGallery.tsx
+++ src/components/ProductGallery.tsx
@@ -18,24 +18,17 @@ const ProductGallery = ({ images }: { images: string[] }) => {
       ]}
       className="rounded-lg mx-8">
       <CarouselContent>
-        <CarouselItem className="flex items-center">
-          <img
-            src="/opengraph.png"
-            width={600}
-            height={600}
-            alt="Product Image"
-            className="object-cover shadow aspect-[3/2] rounded"
-          />
-        </CarouselItem>
-        <CarouselItem className="flex items-center">
-          <img
-            src="/opengraph.png"
-            width={600}
-            height={600}
-            alt="Product Image"
-            className="object-cover shadow aspect-[3/2] rounded"
-          />
-        </CarouselItem>
+        {images.map((image, i) => (
+          <CarouselItem key={i} className="flex items-center">
+            <img
+              src={image}
+              width={600}
+              height={600}
+              alt="Product Image"
+              className="object-cover shadow aspect-[3/2] rounded"
+            />
+          </CarouselItem>
+        ))}
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
