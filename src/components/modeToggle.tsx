8b91817f20d618ee1c64deaa034d40f77da48bbe
Anish <xevenbiswas@gmail.com>
2024-09-14 12:00:21 +0530

    update hero component images and styles; modify mode toggle button background; fix navbar path; adjust team member filtering logic

--- src/components/modeToggle.tsx
+++ src/components/modeToggle.tsx
@@ -23,7 +23,7 @@ export function ModeToggle() {
   return (
     <Button
       variant="outline"
-      className="rounded-full"
+      className="rounded-full bg-background/20"
       size="icon"
       onClick={() => {
         setThemeState(theme === "dark" ? "light" : "dark");
